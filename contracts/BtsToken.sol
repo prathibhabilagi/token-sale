74bc528a09004ff94ffcdf44b5f5e6ffc92661c4
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-16 12:18:53 +0530

    initialize totalSupply

---
+++ contracts/BtsToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.7.4;
+
+contract BtsToken {
+
+    //Constructor
+     uint256 public totalSupply;
+
+    constructor() {
+        totalSupply = 1000000;
+    }
+
+    //Set the total number of tokens
+    //Read the total number of tokens
+}
