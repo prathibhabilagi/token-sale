9ec60d94edf8e25e98e9570a81728b94f7c65590
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-16 12:22:13 +0530

    test the total supply upon deployment

---
+++ test/BtsToken.test.js
@@ -0,0 +1,12 @@
+var BtsToken = artifacts.require("../contracts/BtsToken.sol");
+
+contract("BtsToken", function(accounts) {
+    it('sets the total supply upon deployment', function() {
+        return BtsToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
+        }).then(function(totalSupply) {
+            assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000')
+        })
+    })
+})
