8a52f9381c1eb9232ebb0e1c4dc9d0bf2576f460
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-17 17:39:13 +0530

    Test case to initialize the correct value to  the contract

---
+++ test/BtsTokenSale.test.js
@@ -0,0 +1,27 @@
+var BtsTokenSale = artifacts.require("./BtsTokenSale.sol");
+
+contract ('BtsTokenSale', function(accounts) {
+  
+    var tokenSaleInstance;
+    var tokenPrice = 1000000000000000; //in wei
+
+    it('initializes the contract with the correct values', function() {
+        return BtsTokenSale.deployed().then(function(instance) {
+            tokenSaleInstance = instance;
+            return tokenSaleInstance.address
+        }).then(function(address) {
+            assert.notEqual(address, 0x0, 'has contract address');
+            return tokenSaleInstance.tokenContract();
+        }).then(function(address) {
+            assert.notEqual(address, 0x0, 'has token contract address');
+            return tokenSaleInstance.tokenPrice();
+        }).then(function(price) {
+          assert.equal(price, tokenPrice, 'token price is correct');
+        });
+    });
+    
+});
+
+
+
+
