8f47b9de0e5dfd07b2d95283b0b24ffbdc36ba75
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-17 17:36:27 +0530

    Deploy token and token sale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const BtsToken = artifacts.require("BtsToken");
+const BtsTokenSale = artifacts.require("BtsTokenSale");
 
-module.exports = function (deployer) {
-  deployer.deploy(BtsToken);
+module.exports = function(deployer) {
+  deployer.deploy(BtsToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(BtsTokenSale, BtsToken.address, tokenPrice);
+  });
 };
