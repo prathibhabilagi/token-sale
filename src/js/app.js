3481015357f0fb05ae27e1d285487109a6d86569
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-18 18:52:13 +0530

    Connecting the front end and blockchain using web3 and initialising the token sale address

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,36 @@
-console.log("app.js loaded")
+App = {
+    web3Provider: null,
+    contracts: {},
+
+    init: function() {
+        console.log("App initalized...");
+        return App.initWeb3();
+    },
+    initWeb3: function() {
+        if (typeof web3 !== 'undefined') {
+          // If a web3 instance is already provided by Meta Mask.
+          App.web3Provider = web3.currentProvider;
+          web3 = new Web3(web3.currentProvider);
+        } else {
+          // Specify default instance if no web3 instance provided
+          App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+          web3 = new Web3(App.web3Provider);
+        }
+        return App.initContracts();
+    },
+    initContracts: function() {
+        $.getJSON("BtsTokenSale.json", function(BtsTokenSale) {
+          App.contracts.BtsTokenSale = TruffleContract(BtsTokenSale);
+          App.contracts.BtsTokenSale.setProvider(App.web3Provider);
+          App.contracts.BtsTokenSale.deployed().then(function(BtsTokenSale) {
+            console.log("BTS Token Sale Address:", BtsTokenSale.address);
+          });
+        })
+    }
+}
+
+$(function() {
+    $(window).load(function() {
+        App.init();
+    })
+});
