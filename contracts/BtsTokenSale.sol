9664a181372be9c225f15bf788be5a113f87981c
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-18 15:47:54 +0530

    Added buy tokens and end sale function

--- contracts/BtsTokenSale.sol
+++ contracts/BtsTokenSale.sol
@@ -3,13 +3,37 @@ pragma solidity ^0.7.4;
 import "./BtsToken.sol";
 
 contract BtsTokenSale {
-    address admin;
+    address payable admin;
     BtsToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public  tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(BtsToken _tokenContract, uint256 _tokenPrice) {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public payable{
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+
+        selfdestruct(admin);
     }
 }
