b053090885192f4f485ced908e8bb2320f4d0110
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-19 17:52:49 +0530

    Load account and token contract

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 App = {
     web3Provider: null,
     contracts: {},
+    account: '0x0',
+    loading: false,
+    tokenPrice: 1000000000000000,
+    tokensSold: 0,
+    tokensAvailable: 750000,
 
     init: function() {
         console.log("App initalized...");
@@ -25,7 +30,94 @@ App = {
           App.contracts.BtsTokenSale.deployed().then(function(BtsTokenSale) {
             console.log("BTS Token Sale Address:", BtsTokenSale.address);
           });
+          }).done(function() {
+            $.getJSON("BtsToken.json", function(BtsToken) {
+              App.contracts.BtsToken = TruffleContract(BtsToken);
+              App.contracts.BtsToken.setProvider(App.web3Provider);
+              App.contracts.BtsToken.deployed().then(function(BtsToken) {
+                console.log("BTS Token Sale Address:", BtsToken.address);
+              });
+              App.listenForEvents();
+              return App.render();
+          });
         })
+    },
+
+    // Listen for events emitted from the contract
+    listenForEvents: function() {
+      App.contracts.BtsTokenSale.deployed().then(function(instance) {
+        instance.Sell({}, {
+          fromBlock: 0,
+          toBlock: 'latest',
+        }).watch(function(error, event) {
+          console.log("event triggered", event);
+          App.render();
+        })
+      })
+    },
+
+    render: function() {
+      if (App.loading) {
+        return;
+      }
+      App.loading = true;
+
+      var loader  = $('#loader');
+      var content = $('#content');
+
+      loader.show();
+      content.hide();
+
+     // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#accountAddress').html("Your Account: " + account);
+      }
+    })
+
+      App.contracts.BtsTokenSale.deployed().then(function(instance) {
+        btsTokenSaleInstance = instance;
+        return btsTokenSaleInstance.tokenPrice();
+      }).then(function(tokenPrice) {
+        App.tokenPrice = tokenPrice;
+        $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+        return btsTokenSaleInstance.tokensSold();
+      }).then(function(tokensSold) {
+        App.tokensSold = tokensSold.toNumber();
+        $('.tokens-sold').html(App.tokensSold);
+        $('.tokens-available').html(App.tokensAvailable);
+
+        var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+        $('#progress').css('width', progressPercent + '%');
+
+         // Load token contract
+        App.contracts.BtsToken.deployed().then(function(instance) {
+        btsTokenInstance = instance;
+        return btsTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.bts-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
+  },
+
+    buyTokens: function() {
+      $('#content').hide();
+      $('#loader').show();
+      var numberOfTokens = $('#numberOfTokens').val();
+      App.contracts.BtsTokenSale.deployed().then(function(instance) {
+        return instance.buyTokens(numberOfTokens, {
+          from: App.account,
+          value: numberOfTokens * App.tokenPrice,
+          gas: 500000 // Gas limit
+        });
+      }).then(function(result) {
+        console.log("Tokens bought...")
+        $('form').trigger('reset')
+      });
     }
 }
 
